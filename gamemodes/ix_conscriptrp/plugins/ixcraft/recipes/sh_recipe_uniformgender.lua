5a35540f3d40258dce48579b301ffbd3807a558b
ScratKnapp <hatdescript@yahoo.ca>
2022-09-10 21:06:35 -0500

    Adding ixcraft

---
+++ gamemodes/ix_conscriptrp/plugins/ixcraft/recipes/sh_recipe_uniformgender.lua
@@ -0,0 +1,10 @@
+RECIPE.name = "Re-fit Conscript Uniform"
+RECIPE.description = "Resize a Conscript Uniform to fit females."
+RECIPE.model = "models/props_junk/PopCan01a.mdl"
+RECIPE.requirements = {
+	["conscriptuniform"] = 1,
+}
+RECIPE.results = {
+	["conscriptuniformfemale"] = 1
+
+}
