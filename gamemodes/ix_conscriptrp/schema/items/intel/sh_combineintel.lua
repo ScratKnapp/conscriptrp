56fa51b21aa922fca10958973282f5c2f8db57c6
ScratKnapp <hatdescript@yahoo.ca>
2022-09-13 21:09:10 -0500

    Intel stuff

---
+++ gamemodes/ix_conscriptrp/schema/items/intel/sh_combineintel.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Combine Intelligence Tablet"
+ITEM.model = "models/illusion/eftcontainers/lcdclean.mdl"
+ITEM.description = "A tablet computer detailing local movements from Combine forces. Could be useful to somebody."
+ITEM.category = "Intel"
+ITEM.flag = "3"
+ITEM.price = 5000
+ITEM.width = 1
+ITEM.height = 1
---
+++ gamemodes/ix_conscriptrp/schema/items/intel/sh_resistancereport.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Resistance Reports"
+ITEM.model = "models/illusion/eftcontainers/intel.mdl"
+ITEM.description = "Reports on local Resistance movements. Could be useful to somebody."
+ITEM.category = "Intel"
+ITEM.flag = "3"
+ITEM.price = 5000
+ITEM.width = 1
+ITEM.height = 1
