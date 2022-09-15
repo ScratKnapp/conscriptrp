1c551a819f40c5f67c8795ac02c5e91d6dc9e747
ScratKnapp <hatdescript@yahoo.ca>
2022-09-15 00:54:48 -0500

    ezdurability added

--- gamemodes/ix_conscriptrp/plugins/ezdurability.lua
+++ gamemodes/ix_conscriptrp/plugins/ezdurability.lua
@@ -6,17 +6,16 @@ PLUGIN.desc = "Allows admins easy access to manage suit durability with commands
 
 
 ix.command.Add("CharDamageArmor", {
-	description = "Damage a characters currently equipped armor by the given amount.",
+	description = "Damage a characters currently equipped armor durability by the given amount.",
 	adminOnly = true,
 	arguments = {
 		ix.type.character,
 		ix.type.number,
 	},
 	OnRun = function(self, client, target, damage)
-		local char = target:GetChar()
-		local inventory = char:GetInv()
+		local inventory = target:GetInv()
 		local resItems = {} 
-		local str = " "
+		local str = ""
 
 		for k, v in pairs (inventory:GetItems()) do
 			if(!v:GetData("equip", false)) then continue end --ignores unequipped items
@@ -28,10 +27,9 @@ ix.command.Add("CharDamageArmor", {
 		end
 
 
-		if resItems == nil then 
-			str = str.. client.. " has no suit equipped."
+		if next(resItems) == nil then 
+			str = str.. target:GetName().. " has no suit equipped."
 			return str
-
 		end 
 
 		for k, v in pairs(resItems) do
@@ -39,11 +37,92 @@ ix.command.Add("CharDamageArmor", {
 			local newDura = math.Round(math.Clamp(curDura - damage, 0, 100))
 			v:SetData("durability", newDura)
 
-			str = str.. "reduced " .. target .. " 's " .. v:GetData("name") .. " from " .. curDura .. "% to " .. newDura .. "\n"
+			str = str.. "Reduced the durability of the " .. v.name.. " equipped by " ..target:GetName().. " from " .. curDura.. "% to " .. newDura .. "%"
+	
+		end
+		return str
+	end
+})
+
+
+ix.command.Add("Charrepairarmor", {
+	description = "Repair a characters currently equipped armor durability by the given amount.",
+	adminOnly = true,
+	arguments = {
+		ix.type.character,
+		ix.type.number,
+	},
+	OnRun = function(self, client, target, repair)
+		local inventory = target:GetInv()
+		local resItems = {} 
+		local str = ""
+
+		for k, v in pairs (inventory:GetItems()) do
+			if(!v:GetData("equip", false)) then continue end --ignores unequipped items
+			
+			local res = v.res
+			if (res) then
+				table.insert(resItems, v)
+			end
+		end
+
+		if next(resItems) == nil then 
+			str = str.. target:GetName().. " has no suit equipped."
+			return str
+
+		end 
+
+		for k, v in pairs(resItems) do
+			local curDura = v:GetData("durability", 100)
+			local newDura = math.Round(math.Clamp(curDura + repair, 0, 100))
+			v:SetData("durability", newDura)
+
+			str = str.. "Increased the durability of the " .. v.name.. " equipped by " ..target:GetName().. " from " .. curDura.. "% to " .. newDura .. "%"
+
 		end
 
 		return str
+	end
+})
 
 
+
+ix.command.Add("Charsetarmor", {
+	description = "Set a character's currently equipped armor durability to the given number.",
+	adminOnly = true,
+	arguments = {
+		ix.type.character,
+		ix.type.number,
+	},
+	OnRun = function(self, client, target, newvalue)
+		local inventory = target:GetInv()
+		local resItems = {} 
+		local str = ""
+
+		for k, v in pairs (inventory:GetItems()) do
+			if(!v:GetData("equip", false)) then continue end --ignores unequipped items
+			
+			local res = v.res
+			if (res) then
+				table.insert(resItems, v)
+			end
+		end
+
+		if next(resItems) == nil then 
+			str = str.. target:GetName().. " has no suit equipped."
+			return str
+
+		end 
+
+		for k, v in pairs(resItems) do
+			local curDura = v:GetData("durability", 100)
+			local newDura = math.Round(math.Clamp(newvalue, 0, 100))
+			v:SetData("durability", newDura)
+
+			str = str.. "Set the durability of the " .. v.name.. " equipped by " ..target:GetName().. " from " .. curDura.. "% to " .. newDura .. "%"
+	
+		end
+
+		return str
 	end
 })
