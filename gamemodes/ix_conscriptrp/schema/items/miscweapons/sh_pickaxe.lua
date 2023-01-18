18ee4c3ab18ec4702ad888dff1034ac65d70ce4e
ScratKnapp <hatdescript@yahoo.ca>
2023-01-18 17:30:33 -0600

    Melees galore

---
+++ gamemodes/ix_conscriptrp/schema/items/miscweapons/sh_pickaxe.lua
@@ -0,0 +1,73 @@
+ITEM.name = "Pickaxe"
+ITEM.description = "A double-sided sharp implement used for collecting ores. Has a comfort rubber grip."
+ITEM.model = "models/weapons/tfa_nmrih/w_me_pickaxe.mdl"
+ITEM.class = "tfa_nmrih_pickaxe"
+ITEM.weaponCategory = "melee"
+ITEM.price = 5000
+ITEM.flag = "1"
+ITEM.height = 4
+ITEM.width = 1
+ITEM.category = "Tools"
+ITEM.canAttach = false
+ITEM.repairCost = ITEM.price/100*1
+ITEM.weight = 3
+ITEM.modifier = 5
+ITEM.isPLWeapon = true
+ITEM.Dmg = "1d10+2"
+ITEM.Pen = 2
+ITEM.Special = "UnBal, Tool"
+
+
+function ITEM:OnInstanced(invID, x, y)
+	if !self:GetData("durability") then
+		self:SetData("durability", 10000)
+	end
+end
+
+if (CLIENT) then
+	function ITEM:PaintOver(item, w, h)
+		//Equipsquare
+		if (item:GetData("equip")) then
+			surface.SetDrawColor(110, 255, 110, 255)
+			--surface.DrawRect(w - 14, h - 14, 8, 8)
+		else
+			surface.SetDrawColor(255, 110, 110, 255)
+		end
+
+		surface.SetMaterial(item.equipIcon)
+		surface.DrawTexturedRect(w-23,h-23,19,19)
+
+		//Durability bar
+		if item:GetData("durability") then
+			local dura = item:GetData("durability",10000)
+			if (item:GetOwner():GetWeapon( item.class )) and (item:GetData("equip")) then
+				surface.SetDrawColor( Color( 255, 255, 255, 255 ) )
+				surface.DrawOutlinedRect( 7, h - 15, 41, 9 )
+				if (dura > 0) then
+					surface.SetDrawColor(110, 255, 110, 100)
+					surface.DrawRect(8, h - 14, (dura/10000) * 40, 8)
+				else
+					surface.SetDrawColor(255, 110, 110, 100)
+					surface.DrawRect(8, h - 14, 40, 8)
+				end
+			else
+				surface.SetDrawColor( Color( 255, 255, 255, 255 ) )
+				surface.DrawOutlinedRect( 7, h - 15, 41, 9 )
+				if (dura > 0) then
+					surface.SetDrawColor(110, 255, 110, 100)
+					surface.DrawRect(8, h - 14, (dura/10000) * 40, 8)
+				else
+					surface.SetDrawColor(255, 110, 110, 100)
+					surface.DrawRect(8, h - 14, 40, 8)
+				end
+			end
+		end
+	end
+
+	function ITEM:PopulateTooltip(tooltip)
+		if (self:GetData("equip")) then
+			local name = tooltip:GetRow("name")
+			name:SetBackgroundColor(derma.GetColor("Success", tooltip))
+		end
+	end
+end
--- gamemodes/ix_conscriptrp/schema/items/tools/sh_wrench.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Wrench"
-ITEM.model = "models/illusion/eftcontainers/wrench.mdl"
-ITEM.description = "A double-sided monkey wrench for tightening or loosening stuff."
-ITEM.category = "Tools"
-ITEM.flag = "3"
-ITEM.price = 10000
-ITEM.width = 1
-ITEM.height = 1
