e1ca49733e67f800e72c4fda872a9e7643c1fa30
ScratKnapp <hatdescript@yahoo.ca>
2023-02-04 13:52:24 -0600

    Bandage recipe no workbench

--- gamemodes/ix_conscriptrp/plugins/ixcraft/recipes/sh_craftbandage.lua
+++ gamemodes/ix_conscriptrp/plugins/ixcraft/recipes/sh_craftbandage.lua
@@ -11,12 +11,3 @@ RECIPE.results = {
 }
 RECIPE.flag = "7" 
 
-RECIPE:PostHook("OnCanCraft", function(recipeTable, client)
-	for _, v in pairs(ents.FindByClass("ix_station")) do
-		if (client:GetPos():DistToSqr(v:GetPos()) < 100 * 100) then
-			return true
-		end
-	end
-
-	return false, "You need to be near a workbench."
-end)
