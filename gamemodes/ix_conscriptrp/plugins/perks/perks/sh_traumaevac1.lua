92c1be821fd9ebeae39082301645e93561930e5e
unknown <deadlychopstick123@gmail.com>
2023-04-21 14:26:13 -0500

    Added Double Dose/Trauma Evac

---
+++ gamemodes/ix_conscriptrp/plugins/perks/perks/sh_traumaevac1.lua
@@ -0,0 +1,3 @@
+PERK.name = "Trauma Evac"
+PERK.description = "Leaving someone behind simply isn't an option. When the time comes to get out, you take everyone with you."
+PERK.effect = "When carrying someone, their weight no longer hinders your movement."
