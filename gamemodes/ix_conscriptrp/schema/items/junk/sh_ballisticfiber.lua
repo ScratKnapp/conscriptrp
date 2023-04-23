088f8102256245b0a359d8097e20d838a3ef9e6d
unknown <deadlychopstick123@gmail.com>
2023-04-23 00:48:06 -0500

    Added Sewing kit, Ballistic Fiber, and Patrol Suit Crafting

---
+++ gamemodes/ix_conscriptrp/schema/items/junk/sh_ballisticfiber.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Ballistic Fiber"
+ITEM.model = "models/mosi/fallout4/props/junk/components/ballisticfiber.mdl"
+ITEM.description = "Military Grade material fit for securing and repairing armored material. Usually utilized in the upkeep of plate carriers, however due to a lack of such a material, most Outlands just used what little they find to secure soft-armor to their scrap attire."
+ITEM.category = "junk"
+ITEM.flag = "1"
+ITEM.price = 150
+ITEM.width = 1
+ITEM.height = 1
