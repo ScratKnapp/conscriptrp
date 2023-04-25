1356a6e800f65c6496e8c3bff605403879367fb5
unknown <deadlychopstick123@gmail.com>
2023-04-25 16:31:01 -0500

    Added Trait, Updated Agile Trait

---
+++ gamemodes/ix_conscriptrp/plugins/perks/perks/sh_combatstim1.lua
@@ -0,0 +1,3 @@
+PERK.name = "Combat Stim"
+PERK.description = "Your time and energy is precious, so you’ve learned that you can’t be wasting any of it on the little things."
+PERK.effect = "As a bonus action, you may use applicable medical items on your turn. Ex.) Morphine, Med-gel auto-injector, coagulant auto-injector, adrenaline pen, and painkillers."
