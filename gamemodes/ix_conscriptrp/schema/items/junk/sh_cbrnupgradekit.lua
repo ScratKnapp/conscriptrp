e011c0e7eb94ac890b94eb0795568cffaeb8d031
unknown <deadlychopstick123@gmail.com>
2023-05-04 15:25:53 -0500

    Added Stuff, Crafting and an item

---
+++ gamemodes/ix_conscriptrp/schema/items/junk/sh_cbrnupgradekit.lua
@@ -0,0 +1,8 @@
+ITEM.name = "CBRN Ballistic Carrier"
+ITEM.model = "models/shtokerbox/ground_backpack_ryukzak.mdl"
+ITEM.description = "An equipment pack carrying leftover supplies from a Nuclear-Radiological Survival Hazmat Suit."
+ITEM.category = "junk"
+ITEM.flag = "1"
+ITEM.price = 450
+ITEM.width = 2
+ITEM.height = 2
