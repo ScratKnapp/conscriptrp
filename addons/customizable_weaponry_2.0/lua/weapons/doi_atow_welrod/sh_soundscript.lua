eda4f87fcd471ae84ea6cd66b57a7f3ffba16c1d
unknown <deadlychopstick123@gmail.com>
2023-05-08 19:29:49 -0500

    Welrod ammo adjustment

---
+++ addons/customizable_weaponry_2.0/lua/weapons/doi_atow_welrod/sh_soundscript.lua
@@ -0,0 +1,55 @@
+SWEP.Sounds = {
+	base_draw = {
+		{time = 0, sound = "INS2SHARED_PISTOLDRAW"},
+	},
+
+	base_fire = {
+		{time = 20/30, sound = "DOIWELROD_TWISTOPEN"},
+		{time = 27/30, sound = "DOIWELROD_BOLTBACK"},
+		{time = 34/30, sound = "DOIWELROD_BOLTRELEASE"},
+		{time = 40/30, sound = "DOIWELROD_TWISTCLOSE"},
+	},
+	
+	base_fire_greased = {
+		{time = 18/30, sound = "DOIWELROD_TWISTOPEN"},
+		{time = 25/30, sound = "DOIWELROD_BOLTBACK"},
+		{time = 30/30, sound = "DOIWELROD_BOLTRELEASE"},
+		{time = 36/30, sound = "DOIWELROD_TWISTCLOSE"},
+	},
+
+	base_dryfire = {
+		{time = 0, sound = "DOIWELROD_EMPTY"},
+	},
+
+	base_reload = {
+		{time = 21/30, sound = "DOIWELROD_MAGOUT"},
+		{time = 65/30, sound = "DOIWELROD_MAGIN"},
+	},
+
+	base_reloadempty = {
+		{time = 21/30, sound = "DOIWELROD_MAGOUT"},
+		{time = 65/30, sound = "DOIWELROD_MAGIN"},
+		{time = 85/30, sound = "DOIWELROD_TWISTOPEN"},
+		{time = 93/30, sound = "DOIWELROD_BOLTBACK"},
+		{time = 102/30, sound = "DOIWELROD_BOLTRELEASE"},
+		{time = 107/30, sound = "DOIWELROD_TWISTCLOSE"},
+	},
+
+	iron_fire = {
+		{time = 20/30, sound = "DOIWELROD_TWISTOPEN"},
+		{time = 27/30, sound = "DOIWELROD_BOLTBACK"},
+		{time = 34/30, sound = "DOIWELROD_BOLTRELEASE"},
+		{time = 40/30, sound = "DOIWELROD_TWISTCLOSE"},
+	},
+
+	iron_fire_greased = {
+		{time = 18/30, sound = "DOIWELROD_TWISTOPEN"},
+		{time = 25/30, sound = "DOIWELROD_BOLTBACK"},
+		{time = 30/30, sound = "DOIWELROD_BOLTRELEASE"},
+		{time = 36/30, sound = "DOIWELROD_TWISTCLOSE"},
+	},
+	
+	iron_dryfire = {
+		{time = 0/30, sound = "DOIWELROD_EMPTY"},
+	},
+}
